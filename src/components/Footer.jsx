3436b89b340e9869a56691023e3b0e021e5ff84f
Laura Cruz <lcc4413@gmail.com>
2020-03-24 21:54:25 +0100

    Footer responsive created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "./styles/Footer.scss";
+
+class Footer extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div class="footer-box">
+        <p id="copyright">2020 © We Are Marketing</p>
+        <img
+          class="logo"
+          src="https://hubspot-partners-prod.s3.amazonaws.com/thumbnails/427994/500.jpeg"
+          alt="WAM We Are Marketing"
+        />
+      </div>
+    );
+  }
+}
+
+export default Footer;
